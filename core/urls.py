64bcc440cf0013b9c7c6297aabe624836794f958
kiplah <kiplahvictor27@gmail.com>
2025-06-03 17:46:34 +0300

    updated

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('book/', views.book_appointment, name='book_appointment'),
     path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
+    path('appointments/', views.view_appointments, name='view_appointments'),
 ]
